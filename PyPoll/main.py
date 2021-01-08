a4a4691200fec42349c7ac43e6e416b4c1787b92
thierzon <thierryzonneveld@gmail.com>
2021-01-08 15:39:54 +0800

    Updated version of PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -44,46 +44,32 @@ with open(resources_path) as csvfile:
     # Complete list of candidates who received votes
 
     candidate_list = votes(candidate_data)
-
+    
     candidate = []
-
-    i = 0
 
     for i in candidate_list:
         
         candidate.append(str(i))
 
-    print(candidate)
-
     # Percentage of votes each candidate won
 
     candidate_percentage = []
 
-    i = 0
-
     for i in candidate_list:
         
-        candidate_percentage.append(round((float(candidate_list[i]) / float(total_votes)) * 100, 3))
-
-    print(candidate_percentage)
+        candidate_percentage.append(round((float(candidate_list[i]) / float(total_votes)) * 100, 2))
 
     # Total number of votes each candidate won
 
     candidate_votes = []
 
-    i = 0
-
     for i in candidate_list:
         
-        candidate_votes.append(round(int(candidate_list[i])))
-
-    print(candidate_votes)
+        candidate_votes.append(int(candidate_list[i]))
 
     # Winner of the election based on popular vote
 
-    i = 0
     winner_votes = 0
-    results = dict()
 
     for i in range (len(candidate_votes)):
 
@@ -91,42 +77,38 @@ with open(resources_path) as csvfile:
 
             winner_votes = candidate_votes[i]
             candidate_winner = str(candidate[i])
-    
-    results["name"] = candidate
-    results["percentage"] = candidate_percentage
-    results["votes"] = candidate_votes
-
-    print(candidate_winner)
-
-    print(results)
 
     # Print analysis results to Terminal
 
-    # print("-----------------------------")
-    # print("Election Results")
-    # print("-----------------------------")
-    # print(f"Total votes: {total_votes}")
-    # print("-----------------------------")
-    # print(f{})
-    # print(f{})
-    # print(f{})
-    # print(f{})
-    # print("-----------------------------")
-    # print(f"Winner: {candidate_winner}")
-    # print("-----------------------------")
-    # # Save analysis results to txt file
+    print("-----------------------------")
+    print("Election Results")
+    print("-----------------------------")
+    print(f"Total votes: {total_votes}")
+    print("-----------------------------")
+    print(f"{candidate[0]}: {candidate_percentage[0]}% ({candidate_votes[0]})")
+    print(f"{candidate[1]}: {candidate_percentage[1]}% ({candidate_votes[1]})")
+    print(f"{candidate[2]}: {candidate_percentage[2]}% ({candidate_votes[2]})")
+    print(f"{candidate[3]}: {candidate_percentage[3]}% ({candidate_votes[3]})")
+    print("-----------------------------")
+    print(f"Winner: {candidate_winner}")
+    print("-----------------------------")
 
-    # analysis_path = os.path.join("Analysis", "analysis.txt")
+    # Save analysis results to txt file
 
-    # text_file = open(analysis_path, "w+")
-    # text_file.write("-----------------------------\n")
-    # text_file.write("Financial analysis\n")
-    # text_file.write("-----------------------------\n")
-    # text_file.write(f"Total months: {total_months}\n")
-    # text_file.write(f"Total profit: ${total_profit}\n")
-    # text_file.write(f"Average change in profit: ${average_change}\n")
-    # text_file.write(f"Greatest increase in profit: {increase_month} (${increase_profit})\n")
-    # text_file.write(f"Greatest decrease in profit: {decrease_month} (${decrease_profit})\n")
-    # text_file.write("-----------------------------\n")
-    # text_file.close()
+    analysis_path = os.path.join("Analysis", "analysis.txt")
+
+    text_file = open(analysis_path, "w+")
+    text_file.write("-----------------------------\n")
+    text_file.write("Election Results\n")
+    text_file.write("-----------------------------\n")
+    text_file.write(f"Total votes: {total_votes}\n")
+    text_file.write("-----------------------------\n")
+    text_file.write(f"{candidate[0]}: {candidate_percentage[0]}% ({candidate_votes[0]})\n")
+    text_file.write(f"{candidate[1]}: {candidate_percentage[1]}% ({candidate_votes[1]})\n")
+    text_file.write(f"{candidate[2]}: {candidate_percentage[2]}% ({candidate_votes[2]})\n")
+    text_file.write(f"{candidate[3]}: {candidate_percentage[3]}% ({candidate_votes[3]})\n")
+    text_file.write("-----------------------------\n")
+    text_file.write(f"Winner: {candidate_winner}\n")
+    text_file.write("-----------------------------\n")
+    text_file.close()
 
